9b34a126c223971c82419e6ff934c6b1c6465f64
Diganta999 <digantasikder123@gmail.com>
2026-05-07 15:53:53 +0600

    Problem 6

--- solutions.ts
+++ solutions.ts
@@ -42,6 +42,30 @@ const toggleReadStatus =(book:Book)=>{
         isRead:true}
 };
 
+// Problem 6  
+
+class Person{
+    name:string;
+    age:number; 
+    constructor(name:string,age:number){
+        this.name = name;
+        this.age= age;
+    }
+}
+
+class Student extends Person{
+    grade:string;
+    constructor(name:string,age:number ,grade:string) {
+        super(name,age)
+        this.grade= grade;
+        
+    }
+    getDetails():string{
+        return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+    }
+};
+
+
 
 
 
