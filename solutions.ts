0a9d08e44f36aa9af2e5335a51bffca1be0b13b5
Diganta999 <digantasikder123@gmail.com>
2026-05-07 15:42:35 +0600

    Problem 4

--- solutions.ts
+++ solutions.ts
@@ -22,3 +22,12 @@ const checkType = (input:StringOrNumber):string=>{
     return "Number";
 };
 
+// Problem 4 
+
+const getProperty =<T,K extends keyof T>(obj:T, key:K):T[K]=>{
+    return obj[key];
+
+};
+
+
+
